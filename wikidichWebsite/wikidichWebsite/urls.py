d0855a31be20d216c7b68ea01d3384a77078308f
hvrlxy <hle5@gustavus.edu>
2022-07-04 21:53:55 +0700

    style the main page

--- wikidichWebsite/wikidichWebsite/urls.py
+++ wikidichWebsite/wikidichWebsite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('wikidthConverter/', include('wikidthConverter.urls')),
-    path('test_template/', include('test_template.urls'))
+    path('test_template/', include('test_template.urls')),
+    path('booklist/', include('booklist.urls')),
 ]
