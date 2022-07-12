5c61fd22848f9ebb442e8fe1f63d10bc1b77273a
hvrlxy <hle5@gustavus.edu>
2022-07-13 00:05:26 +0700

    generate pdf file and add to file system

--- wikidichWebsite/wikidthConverter/views.py
+++ wikidichWebsite/wikidthConverter/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponse
 from django import forms
 from wikidichConverter.convert import *
 from wikidichConverter.parse_mainpage import *
+import os
+from django.conf import settings
+from django.conf import settings
+
+# book_url = "pdfs/sample_book.pdf"
+# is_file = os.path.exists(os.path.join(settings.MEDIA_ROOT, book_url))
 
 class LinkForm(forms.Form):
     url = forms.URLField(label='Book\'s URL', max_length=10000)
@@ -16,25 +22,20 @@ books = ['book1', 'book2', 'book3']
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        form = LinkForm(request.POST)
+        if form.is_valid():
+            url = form.cleaned_data['url']
+            file_format = form.cleaned_data['file_format']
+            book_name = ParseMainPage(url).get_book_name()
+            book_name = book_name.replace(' ', '_')
+            convert_pdf(url, os.path.join(settings.MEDIA_ROOT, 'pdfs/' + book_name + '.pdf'))
+            books.append(book_name)
+        else:
+            return render(request, "wikidthConverter/index.html", {"form": form})    
     return render(request, "wikidthConverter/index.html", {"form": LinkForm()})
 
 
 # Create your views here.
 def all_books(request):
     return render(request, 'wikidthConverter/booklist.html', {'books': books})
-
-
-def generate_file(request):
-    if request.method == 'POST':
-        form = LinkForm(request.POST)
-        if form.is_valid():
-            url = form.cleaned_data['url']
-            file_format = form.cleaned_data['file_format']
-            book_name = book_name = ParseMainPage(url).get_book_name()
-            file_name = book_name + '.' + file_format
-            if file_format == 'pdf':
-                convert_pdf(url, "static/pdfs/" + file_name)
-            return HttpResponse("<h1>Generating file for " + url + "</h1>")
-    else:
-        form = LinkForm()
-    return render(request, 'wikidthConverter/download.html', {'file_name': file_name})
